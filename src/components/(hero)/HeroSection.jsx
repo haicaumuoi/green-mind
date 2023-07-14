d02bfc0b8f28457496fb7f9cf952cb6c59b090aa
HAIN😎\HP VICTUS <haianh12122002@gmail.com>
2023-07-14 15:09:33 +0700

    a

--- src/components/(hero)/HeroSection.jsx
+++ src/components/(hero)/HeroSection.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from "react";
 import ShoppingCartIcon from "../icon/ShoppingCartIcon";
 import Image from "next/image";
@@ -5,8 +7,25 @@ import arrow1 from "./heroImg/arrow1.png";
 import arrow2 from "./heroImg/arrow2.png";
 import chatZone from "./heroImg/chatZone.png";
 import treePot from "./heroImg/treePot.png";
+import axios from "axios";
 
 const HeroSection = () => {
+
+  const url = 'https://d31w5oj2o0nlp6.cloudfront.net/images/bluetooth-device-type/image/08a9bdf6-d280-42f6-aeb8-2c2fb851e969.png';
+
+  const cookie = `CloudFront-Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaHR0cHM6Ly8qIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNjg5NjA2NzQxfX19XX0_; CloudFront-Signature=dFgzjqp81SaKCRbIeVE8ym9YUmKNLhCehphHThtICdwWcZ4o6cC09oEPmWNJoAOxbp-SG9L1auQKx3kCHM9yNd97YUU4Sx9KMcNPLLyss5PkbS2tdaqAOAMMkyrdf5evLkyT~3rp2YbdjAY9FtVSaGnfduZXoaTnkdVVu5ZobYjdNHr0dtDbhYtSn67jm5fp5X1rk65kbJCqf2s~GT9065uPl5kCfN658g53ORUVJTWtEfoMMJ7CQ0N5X-vRgAauziowxO-8jzhArOX2M8W9S~dMrCaYDEDbyiE~hMRZTk~XZGnxvZ6IkeopuoHhlNjKu9lGsqRO51mCJh4vrVQ5CA__; CloudFront-Key-Pair-Id=K1R22U7UX4WVU8`;
+
+  const callApiWithCookie = () => {
+    axios.get(url, { headers: { Cookie: cookie } })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+
   return (
     <div className="bg-[#C1DCDC] w-full h-[30rem] rounded-3xl p-10 flex">
       <div className="w-2/5 flex flex-col space-y-14 pl-4">
@@ -54,7 +73,7 @@ const HeroSection = () => {
         <div className="absolute right-0 -top-9">
           <Image src={arrow2} alt="arrow2" />
         </div>
-        <div className="absolute -bottom-10 left-56 z-10">
+        <div className="absolute -bottom-10 left-56 z-10 cursor-pointer" onClick={callApiWithCookie} >
           <Image src={treePot} alt="treePot" />
         </div>
         <div className="absolute -bottom-10 left-56">
